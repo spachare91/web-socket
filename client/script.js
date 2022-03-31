16169e4d192c8aeb2080d7ce2df948e62e0fef14
spachare91 <spachare91@gmail.com>
2022-03-31 11:57:27 +0530

    files updated

--- client/script.js
+++ client/script.js
@@ -1,7 +1,26 @@
+// functio to get individua; io
+import {io} from 'socket.io-client'
+
 const joinRoomButton  = document.getElementById("room-button")
 const messageInput  = document.getElementById("message-input")
 const roomInput  = document.getElementById("room-input")
 const form = document.getElementById("form")
+
+// connect wtih server.....
+const socket=io('http://localhost:3000')
+
+// creating event which runs when we connect {listeninng event comming down from server... "ON"}
+socket.on('connect',()=>{
+    displayMessage(`connected with id : ${socket.id}`)
+})
+
+socket.on("receive-message",message=>{
+    displayMessage(message)
+})
+
+// FOR CLIENT SIDE... ON MEANS COMING FROM SERVER
+// EMIT MEANS SENDING BACK TO SERVER....
+// socket.emit("custom-event",)
 
 form.addEventListener("submit",e=>{
     e.preventDefault()
@@ -10,11 +29,15 @@ form.addEventListener("submit",e=>{
 
     if(message=== "") return 
     displayMessage(message)
+    socket.emit("send-message",message,room)
     messageInput.value=""
 })
 
 joinRoomButton.addEventListener("click",()=>{
     const room=roomInput.value
+    socket.emit("join-room",room,message=>{
+        displayMessage(message)
+    })
 })
 
 function displayMessage(message){
--- server/index.js
+++ server/index.js
@@ -1,13 +1,35 @@
-// require socket.io and pass port number
+// use cors so that do different ports can communicate....
 
-const io = require('socket.io')(3000,{
+const io=require('socket.io')(3000,{
     cors:{
-        
+        origin:["http://localhost:8080"]
     }
 })
 
+//this function in called whenever a client connects
+// ON for server means something is comming from client
+// EMIT means to send to all clients
 
-// runs every single time when client connects to a server....
 io.on('connection',socket=>{
-    console.log(socket.id)
+    console.log(`connected with id : ${socket.id}`);
+    socket.on("send-message",(message,room)=>{
+        // send message comming from an cient...to other clients
+        if(room===''){
+            socket.broadcast.emit("receive-message",message)
+        }else{
+            // useful for 1-1 communication....
+            socket.to(room).emit("receive-message",message)
+        }
+        
+        //console.log(message)
+    })
+
+    socket.on("join-room",(room,cb)=>{
+        // joining to a particuar room....
+        //one use can join mulitple rooms...
+        socket.join(room)
+        // send from client to server & server back to client......
+        cb(`JOined room :${room}`)
+
+    })
 })
