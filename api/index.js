0ab29eb3dc0a9d452d2b924d3895eaabb39f56bc
spachare91 <spachare91@gmail.com>
2022-04-04 16:26:56 +0530

    z

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 const express=require("express")
 
 const app=express()
+const { sequelize, Conversation ,Message }= require('./models')
 
 
 app.get('/',(req,res)=>{
     req.send("hello....")
 })
 
-app.listen(3000,()=>{
-    console.log("connected to server....");
+app.listen(5000, async()=>{
+
+    console.log("server connected to http://localhost:5000")
+    await sequelize.authenticate()
+    console.log("database connected..");
 })
