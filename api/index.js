45ae02f2a559e04f35b2a01c9879f0006a4ee0f3
spachare91 <spachare91@gmail.com>
2022-04-04 16:23:46 +0530

    file

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express=require(express)
+
+const app=express()
+
+
+app.get('/',(req,res)=>{
+    req.send("hello....")
+})
+
+app.listen(3000,()=>{
+    console.log("connected to server....");
+})
