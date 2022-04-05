e7fa94258629ecc6c8a5dbad6f815f42fb07d8b0
spachare91 <spachare91@gmail.com>
2022-04-05 07:55:17 +0530

    chages done

--- client/script.js
+++ client/script.js
@@ -5,15 +5,37 @@ const joinRoomButton  = document.getElementById("room-button")
 const messageInput  = document.getElementById("message-input")
 const roomInput  = document.getElementById("room-input")
 const form = document.getElementById("form")
-
+const userid="d51d4a56-4ed1-45ce-b4e9-1e57a2e2d260"
+const convoid="92c1410b-75ad-428e-bf74-08f62fb84872"
 // connect wtih server.....
 const socket=io('http://localhost:3000')
-
+let online_users=[]
 // creating event which runs when we connect {listeninng event comming down from server... "ON"}
 socket.on('connect',()=>{
 
     displayMessage(`connected with id : ${socket.id}`)
-    fetch('http://localhost:5000/92c1410b-75ad-428e-bf74-08f62fb84872', {
+   // adding user id
+    socket.emit("addUser",userid)
+    socket.on("getUsers", (users) => {
+        if(userid!="d51d4a56-4ed1-45ce-b4e9-1e57a2e2d261"){
+            console.log("user is connected...");
+            online_users=users.filter((user)=>{
+                return user.userId=="d51d4a56-4ed1-45ce-b4e9-1e57a2e2d261"
+            })
+            console.log(online_users);
+        }
+        else{
+            console.log("admin is connected...");
+            //console.log("online users:"+online_users);
+            online_users=users.filter((user)=>{
+                return user.userId!="d51d4a56-4ed1-45ce-b4e9-1e57a2e2d261"
+            })
+            console.log(online_users);
+        }
+      });
+
+
+    fetch(`http://localhost:5000/${convoid}`, {
     method: 'GET', // or 'PUT'
     headers: {
         'Content-Type': 'application/json',
@@ -31,22 +53,22 @@ socket.on('connect',()=>{
     });
 
     // show all users
-    fetch('http://localhost:5000/getusers', {
-        method: 'GET', // or 'PUT'
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        })
-        .then(response => response.json())
-        .then(data => {
-        console.log('Success:', data);
-        for(let i=0;i<data.length;i++){
-            displayMessage(data[i].member[0])
-        }
-        })
-        .catch((error) => {
-        console.error('Error:', error);
-        });
+    // fetch('http://localhost:5000/getusers', {
+    //     method: 'GET', // or 'PUT'
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //     console.log('Success:', data);
+    //     for(let i=0;i<data.length;i++){
+    //         displayMessage(data[i].member[0])
+    //     }
+    //     })
+    //     .catch((error) => {
+    //     console.error('Error:', error);
+    //     });
 
 
 
@@ -67,34 +89,36 @@ form.addEventListener("submit",e=>{
 
     if(message=== "") return 
     displayMessage(message)
-    socket.emit("send-message",message,room)
+    if(online_users.length==0){
+        console.log("admin is offline..save msg");
+    }else{
+        console.log("admin is online..save msg");
+        console.log("userid: ",online_users[0].userId);
+        console.log("socketId: ",online_users[0].socketId);
+        socket.emit("send-message",message,room)
+    }
+
     let data={
-        "conversationId":"92c1410b-75ad-428e-bf74-08f62fb84872",
-        "senderId":"d51d4a56-4ed1-45ce-b4e9-1e57a2e2d260",
+        "conversationId":convoid,
+        "senderId":userid,
         "message":message
     }
 
-    // fetch('',{
-    //     body:data,
-    //     method:"POST"
-    // })
 
-    // const data = { username: 'example' };
-
-fetch('http://localhost:5000/addmsg', {
-  method: 'POST', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+    fetch('http://localhost:5000/addmsg', {
+    method: 'POST', // or 'PUT'
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Success:', data);
+    })
+    .catch((error) => {
+    console.error('Error:', error);
+    });
 
 
     messageInput.value=""
@@ -104,9 +128,6 @@ fetch('http://localhost:5000/addmsg', {
 
 joinRoomButton.addEventListener("click",()=>{
     const room=roomInput.value
-    socket.emit("join-room",room,message=>{
-        displayMessage(message)
-    })
 })
 
 function displayMessage(message){
